a9d80e8d464f7fd1fabb41ceceda4702879c6e1b
Rich Knight <rich@rich-knight.com>
2015-07-03 09:24:17 +0100

    created basic input form

--- app/app.jsx
+++ app/app.jsx
@@ -1,12 +1,12 @@
 var React = require('react');
-//var RequestForm = require('./components/feature-request/request-form.jsx');
+var RequestForm = require('./components/feature-request/request-form.jsx');
 //var ErrorPage = require('./components/error/error.jsx');
 
 var App = React.createClass({
     render: function () {
         return (
             <div id="app">
-                <h1>Hello World</h1>
+                <RequestForm />
             </div>
         )
     }
--- app/components/feature-request/request-form.jsx
+++ app/components/feature-request/request-form.jsx
@@ -2,6 +2,28 @@ var React = require("react");
 
 module.exports = React.createClass({
     render: function() {
-        return "";
+        return (
+                <form>
+                    <fieldset id="userDetails">
+                        <legend>Your details</legend>
+                        <label for="firstName">First Name</label>
+                        <input id="firstName"></input>
+                        <label for="surname">Surname</label>
+                        <input id="surname"></input>
+                        <label for="email">Email Address</label>
+                        <input id="email"></input>
+                    </fieldset>                    
+                    <fieldset id="description">
+                        <legend>Describe your feature</legend>
+                        <label for="title">Title</label>
+                        <input id="title" />
+                        <label for="card-body">Full Description</label>
+                        <textArea id="card-body"></textArea>
+                        <aside>(<a href="http://daringfireball.net/projects/markdown/">Markdown</a> formatting is supported)</aside>
+                    </fieldset>
+                    <button type="submit">Submit your request</button>
+                    <button>Clear</button>
+                </form>
+               );
     }
 });
