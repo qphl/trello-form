f1109a22389ef887f760e1c8ad9ef64573a0e1de
Rich Knight <rich@rich-knight.com>
2015-07-06 08:59:44 +0100

    sample config added

---
+++ config.sample.js
@@ -0,0 +1,6 @@
+window.config = {
+    organisation: "Your Organisation Name",
+    trelloAppKey: "Your Trello Developr API Key",
+    trelloUserToken: "Your Trello User Token",
+    trelloListId: "List ID of the trello list you want to submit into"
+};
