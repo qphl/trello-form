7cc5cd172c232bb4b04a003069987691597285f5
Rich Knight <rich@rich-knight.com>
2018-05-10 11:43:33 +0100

    Fix 0 returned by issueList

--- app/components/request-form/issue-list.jsx
+++ app/components/request-form/issue-list.jsx
@@ -5,7 +5,7 @@ const issueList = ({ issues }) => {
   if (issues.length > 0) {
     return <ul id="validation-issues">{issues.map(i => <li>{i}</li>)}</ul>;
   }
-  return 0;
+  return null;
 };
 
 issueList.propTypes = {
