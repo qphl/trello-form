a088ce4545575cc1fb1c63a8a3d4b5eb13774511
Rich Knight <rich@rich-knight.com>
2018-05-10 11:38:30 +0100

    Deleted weird space

--- app/components/error/error.jsx
+++ app/components/error/error.jsx
@@ -3,7 +3,6 @@ const PropTypes = require("prop-types");
 
 const errorView = ({ text, onReturn }) => (
   <div className="container" id="error">
-    {" "}
     <h1>An error occurred submitting your request</h1>
     <pre>{text}</pre>
     <button onClick={onReturn}>Go Back</button>
